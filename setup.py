1b6fdf933263069b912cada6307b289d6dc1fcce
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-05-15 15:46:14 +0100

    Setup.py/entry point

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='holst',
+  version='0.1',
+  description='Generate iptables files from yaml dsl',
+  author='Alex Wilson',
+  author_email='a.wilson@alumni.warwick.ac.uk',
+  packages=['holst'],
+  entry_points = {
+  'console_scripts': ['holst=holst:main']
+  },
+  zip_safe=False)
