bc0e03af7f3c5acbb0ce20c136249cc7ed2a0e86
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-05-15 11:17:30 +0100

    Skeleton for rule

--- holst/core/service.py
+++ holst/core/service.py
@@ -1,4 +1,13 @@
+class Rule():
+
+  def __init__(self, obj):
+    self.rule = "-A INPUT -p tcp -m tcp --dport 80 -j ACCEPT"
+
+  def to_rule(self):
+    return self.rule
+
 class Service():
 
   def __init__(self, name, obj):
-    pass
+    r = Rule(obj)
+    self.rules = [r]
--- test/test_service.py
+++ test/test_service.py
@@ -11,11 +11,11 @@ class ServiceTest(unittest.TestCase):
     parser = Parser()
 
     out = parser.parse("""
-      foo:
+      example:
         type: service
     """)
     assert len(out["services"]) == 1
-    assert isinstance(out["services"]["foo"], Service)
+    assert isinstance(out["services"]["example"], Service)
 
   @raises(Exception)
   @test
@@ -23,8 +23,24 @@ class ServiceTest(unittest.TestCase):
     parser = Parser()
 
     out = parser.parse("""
-      foo:
+      example:
         type: host
         services:
           - notcreated
     """)
+
+  @test
+  def parse_rule(self):
+    parser = Parser()
+
+    out = parser.parse("""
+      example:
+        type: service
+        rule:
+          accept: ["tcp", 80]
+    """)
+
+    example_service = out["services"]["example"]
+    assert len(example_service.rules) == 1
+    assert example_service.rules[0].to_rule() == "-A INPUT -p tcp -m tcp --dport 80 -j ACCEPT"
+    
