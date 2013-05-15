df3429aa9a87faff48ff6203ee26d9ead934c915
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-05-15 16:01:14 +0100

    Tie entry point into parser

--- holst/__init__.py
+++ holst/__init__.py
@@ -1,4 +1,24 @@
 import sys
+from holst.parser import Parser
 
 def main():
-  print "HELLO WORLD!"
+  if len(sys.argv) != 3:
+    print "Usage: holst <template-file> <host-name>"
+    sys.exit(0)
+
+  filename = sys.argv[1]
+  hostname = sys.argv[2]
+
+  rules = []
+
+  try:
+    with open(filename) as template_file:
+      parser = Parser()
+      parser.parse(template_file.read())
+      rules = parser.get_rules_for(hostname)
+  except IOError:
+    print 'Cannot access file: %s' % filename
+    sys.exit(1)
+
+  for rule in rules:
+    print rule
