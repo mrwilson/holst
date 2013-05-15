c302c5a8b0da3c8f992b271b63340e8663a502b8
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2013-05-15 12:22:27 +0100

    Refactored rule to its own file

--- holst/core/__init__.py
+++ holst/core/__init__.py
@@ -1,2 +1,3 @@
 from host import *
 from service import *
+from rule import *
--- holst/core/service.py
+++ holst/core/service.py
@@ -1,18 +1,4 @@
-class Rule(object):
-
-  def __init__(self, obj):
-    self.operation, rule = obj.popitem()
-    self.protocol = rule[0]
-    self.dport = rule[1]
-
-  def to_rule(self):
-    return "-A INPUT -p %s -m %s --dport %d -j %s" % (self.protocol, self.protocol, self.dport, self.operation.upper())
-
-  def __hash__(self):
-    return hash(self.dport)
-
-  def __eq__(self,other):
-    return self.dport == other.dport
+from rule import Rule
 
 class Service():
 
